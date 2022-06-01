f08691a180c32b1c969cc043312bb42edec0669a
ccumaco <carloscumaco5@gmail.com>
2022-06-01 13:15:07 -0500

    adding vuetify

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,15 @@
 <template>
-  <div></div>
+  <hello-world />
 </template>
 
 <script>
-// @ is an alias to /src
+  import HelloWorld from '../components/HelloWorld'
 
-export default {
-  name: 'HomeView',
-  components: {
+  export default {
+    name: 'Home',
+
+    components: {
+      HelloWorld,
+    },
   }
-}
 </script>
